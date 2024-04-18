b5a6731794415acf487616288e0f2f7c84634954
cbernot <camil.bernot.cb@gmail.com>
2024-04-18 11:14:08 +0200

    [WIP]

--- backend/user_stats/docker/django/entrypoint.sh
+++ backend/user_stats/docker/django/entrypoint.sh
@@ -2,6 +2,8 @@
 
 # Wait for the database to be ready
 echo "Waiting for database..."
+echo "Host: $SQL_HOST"
+echo "Port: $SQL_PORT"
 while ! nc -z $SQL_HOST $SQL_PORT; do
   sleep 0.1
 done
@@ -10,5 +12,5 @@ echo "Database ready"
 python manage.py flush --no-input
 python manage.py migrate
 
-#exec "python" "manage.py" "runserver" "0.0.0.0:8000"
-exec "gunicorn" "user_stats.wsgi:application" "--bind" "0.0.0.0:8000"
+exec "python" "manage.py" "runserver" "0.0.0.0:8000"
+# exec "gunicorn" "user_stats.wsgi:application" "--bind" "0.0.0.0:8000"
