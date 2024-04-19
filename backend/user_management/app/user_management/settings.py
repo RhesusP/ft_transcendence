3a234ccb91b30ae3fcd50c27dab3015024e90a8e
cbernot <camil.bernot.cb@gmail.com>
2024-04-19 10:59:04 +0200

    ✅ Tanguy's code as microservice

--- backend/user_management/app/user_management/settings.py
+++ backend/user_management/app/user_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8x1x4u@eq)^fj!ow2^2hd5en$o9lb$eay$&_q&^9&s_hqke+h%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'corsheaders',
+	'users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('SQL_ENGINE'),
+        'NAME': os.environ.get('POSTGRES_DB_USER_MANAGEMENT'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
     }
 }
 
@@ -115,9 +123,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'	# TODO check this
+
+CORS_ORIGIN_ALLOW_ALL = True  # allow all frontend ports to access app
+CORS_ALLOW_CREDENTIALS = True  # for frontend to get the jwt cookies
+
+REST_FRAMEWORK = {
+    
+}
--- backend/user_management/docker/django/entrypoint.sh
+++ backend/user_management/docker/django/entrypoint.sh
@@ -10,7 +10,8 @@ done
 echo "Database ready"
 
 python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 
-exec "gunicorn" "user_stats.wsgi:application" "--bind" "0.0.0.0:8000"
+exec "gunicorn" "user_management.wsgi:application" "--bind" "0.0.0.0:8000"
